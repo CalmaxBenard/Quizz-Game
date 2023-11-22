47c7a281c2fe1a8e0e676b4e7510dc482ee078b2
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-22 13:14:12 +0300

    execute the quizbrain module

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -7,6 +8,13 @@ for question in question_data:
     new_question = Question(question["text"], question["answer"])
     question_bank.append(new_question)
 
+quizz = QuizBrain(question_bank)
 
+game_on = True
 
-
+while game_on:
+    if quizz.still_has_questions():
+        quizz.next_question()
+    else:
+        print(f"You've completed the quiz.\nYour final score is: {quizz.score}/12")
+        game_on = False
